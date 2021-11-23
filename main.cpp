ccca2801fae48975bdf23de32cd9f945ab6cfa1c
Undyne <87874181+undynes@users.noreply.github.com>
2021-11-23 13:59:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
-#include
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
